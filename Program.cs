317a034230690b8de98d667e570864d2113d2573
Trapeznikov Vasyl <s2200711@BC-C02J61E5Q6LR-MacBook-Air.local>
2022-10-05 14:34:29 +0300

    addallfiles

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using Faker;
+internal class Program
+{
+    private static void Main(string[] args)
+    
+    {
+        for (int i =1; i<=3; i++)
+    {
+        var name = Faker.Name.FullName();
+        var city = Faker.Address.City();
+        var id = Faker.RandomNumber.Next(100000000);
+
+        Console.WriteLine($"Hello, {name} from {city} id {id}");
+        }
+    }
+}
